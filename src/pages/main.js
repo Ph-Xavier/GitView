a2df7bff28315e15302f90ccdae75bad07216ce0
Pedro Xavier <pedro_xavier9@outlook.com>
2026-03-02 20:25:44 -0300

    feat: começando a pagina main

---
+++ src/pages/main.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Keyboard, ActivityIndicator } from "react-native";
+import Icon from "@expo/vector-icons/MaterialIcons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import api from "../services/api.js";
+import {} from "../styles.js";
+
+export default class Main extends Component {
+  render() {
+    return <></>;
+  }
+}
